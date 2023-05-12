636809ef4584939729cd66f8efc751d172e30201
Freewind <nowindlee@gmail.com>
2023-05-12 21:21:46 +0800

    typescript-vite-onhide-false-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Vite Tippyjs Demo
+TypeScript Vite Tippyjs onHide false Demo
 ===========================
+
+如果onHide返回false，则不会关闭。但是'tippy.destroy'会无视它。
 
 ```
 npm install
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,17 @@ import './index.css';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-const button = document.querySelector("#main button");
-tippy(button!, {
-    content: 'Hello, tippy'
+const hoverMe = document.querySelector("#hoverMe");
+const destroy = document.querySelector("#destroy");
+
+const tippyInstance = tippy(hoverMe!, {
+    content: 'Hello, tippy',
+    onHide: () => {
+        console.log("### onHide")
+        return false;
+    },
+})
+
+destroy!.addEventListener('click', () => {
+    tippyInstance.destroy();
 })
